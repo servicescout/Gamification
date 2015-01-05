158217194940f53c1f716041df10ca7510d1e0e7
Tyler Flanders <tflanders@servicescout.com>
2015-01-05 13:18:20 -0800

    Updated level calculation

--- lib/game/level/Helper.php
+++ lib/game/level/Helper.php
@@ -6,16 +6,16 @@ class Helper
 {
   public function getLevelForXP($xp)
   {
-    return floor(pow($xp + 1, 0.4));
+    return floor(pow($xp, 0.8) / 10) + 1;
   }
-  
+
   public function getPercentToNextLevel($xp)
   {
-    return floor((pow($xp + 1, 0.4)) * 100) % 100;
+    return floor(((pow($xp, 0.8) / 10) + 1) * 100) % 100;
   }
-  
+
   public function getXPNeededForNextLevel($xp)
   {
-    return ceil(pow($this->getLevelForXP($xp) + 1, 10 / 4)) - ($xp + 1);
+    return ceil(floor(pow($this->getLevelForXP($xp) * 10, 10 / 8))) - ($xp - 1);
   }
 }
