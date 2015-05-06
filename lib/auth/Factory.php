7990ac68e69046964e9eee9828b7411058d74dcc
Brian Klima <bklima@servicescout.com>
2015-05-06 13:58:55 -0700

    Added back special handling for apache's authorization behavior.

--- lib/auth/Factory.php
+++ lib/auth/Factory.php
@@ -11,9 +11,20 @@ class Factory
    */
   public function fromApp(\Slim\Slim $app)
   {
+    // standard Authorization
     if (isset($_SERVER['HTTP_AUTHORIZATION']))
     {
       return new Token($_SERVER['HTTP_AUTHORIZATION']);
+    }
+    // Apache-specific Authorization behavior (stripped from $_SERVER by default)
+    elseif (function_exists('apache_request_headers'))
+    {
+      $headers = apache_request_headers();
+
+      if (isset($headers['Authorization']))
+      {
+        return new Token($headers['Authorization']);
+      }
     }
 
     return new Web();
